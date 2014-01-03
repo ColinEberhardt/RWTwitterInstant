2ecd885756bf20add2efbdcdba2e9220ea96648d
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2014-01-03 21:34:43 +0000

    Requested access to twitter

--- TwitterInstant/RWSearchFormViewController.m
+++ TwitterInstant/RWSearchFormViewController.m
@@ -10,12 +10,23 @@
 #import "RWSearchResultsViewController.h"
 #import "RACEXTScope.h"
 #import <ReactiveCocoa/ReactiveCocoa.h>
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
+
+typedef NS_ENUM(NSInteger, RWTwitterInstantError) {
+  RWTwitterInstantErrorAccessDenied
+};
+
+static NSString * const RWTwitterInstantDomain = @"TwitterInstant";
 
 @interface RWSearchFormViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *searchText;
 
 @property (strong, nonatomic) RWSearchResultsViewController *resultsViewController;
+
+@property (strong, nonatomic) ACAccountStore *accountStore;
+@property (strong, nonatomic) ACAccountType *twitterAccountType;
 
 @end
 
@@ -41,7 +52,46 @@
       @strongify(self)
       self.searchText.backgroundColor = color;
     }];
+  
+  self.accountStore = [[ACAccountStore alloc] init];
+  self.twitterAccountType = [self.accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+
+  [[self requestAccessToTwitterSignal]
+    subscribeNext:^(id x) {
+      NSLog(@"Access granted");
+    } error:^(NSError *error) {
+      NSLog(@"An error occurred: %@", error);
+    }];
 }
+
+- (RACSignal *)requestAccessToTwitterSignal {
+  
+  // 1 - define an error
+  NSError *accessError = [NSError errorWithDomain:RWTwitterInstantDomain
+                                             code:RWTwitterInstantErrorAccessDenied
+                                         userInfo:nil];
+  
+  // 2 - create the signal
+  @weakify(self)
+  return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+    // 3 - request access to twitter
+    @strongify(self)
+    [self.accountStore
+       requestAccessToAccountsWithType:self.twitterAccountType
+         options:nil
+      completion:^(BOOL granted, NSError *error) {
+          // 4 - handle the response
+          if (!granted) {
+            [subscriber sendError:accessError];
+          } else {
+            [subscriber sendNext:nil];
+            [subscriber sendCompleted];
+          }
+        }];
+    return nil;
+  }];
+}
+
 
 - (BOOL)isValidSearchText:(NSString *)text {
   return text.length > 2;

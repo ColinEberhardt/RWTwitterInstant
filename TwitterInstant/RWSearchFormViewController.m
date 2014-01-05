be936302a91f81ab4d5c43ba46e46949a308d409
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2014-01-05 07:58:40 +0000

    twitter search

--- TwitterInstant/RWSearchFormViewController.m
+++ TwitterInstant/RWSearchFormViewController.m
@@ -14,7 +14,9 @@
 #import <Social/Social.h>
 
 typedef NS_ENUM(NSInteger, RWTwitterInstantError) {
-  RWTwitterInstantErrorAccessDenied
+  RWTwitterInstantErrorAccessDenied,
+  RWTwitterInstantErrorNoTwitterAccounts,
+  RWTwitterInstantErrorInvalidResponse
 };
 
 static NSString * const RWTwitterInstantDomain = @"TwitterInstant";
@@ -56,7 +58,7 @@ static NSString * const RWTwitterInstantDomain = @"TwitterInstant";
   self.accountStore = [[ACAccountStore alloc] init];
   self.twitterAccountType = [self.accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
 
-  [[[[self requestAccessToTwitterSignal]
+  [[[[[self requestAccessToTwitterSignal]
     then:^RACSignal *{
       @strongify(self)
       return self.searchText.rac_textSignal;
@@ -64,6 +66,10 @@ static NSString * const RWTwitterInstantDomain = @"TwitterInstant";
     filter:^BOOL(NSString *text) {
       @strongify(self)
       return [self isValidSearchText:text];
+    }]
+    flattenMap:^RACStream *(NSString *text) {
+      @strongify(self)
+      return [self signalForSearchWithText:text];
     }]
     subscribeNext:^(id x) {
       NSLog(@"%@", x);
@@ -99,6 +105,69 @@ static NSString * const RWTwitterInstantDomain = @"TwitterInstant";
     return nil;
   }];
 }
+
+- (SLRequest *)requestforTwitterSearchWithText:(NSString *)text {
+  NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/search/tweets.json"];
+  NSDictionary *params = @{@"q" : text};
+  
+  SLRequest *request =  [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                           requestMethod:SLRequestMethodGET
+                                                     URL:url
+                                              parameters:params];
+  return request;
+}
+
+- (RACSignal *)signalForSearchWithText:(NSString *)text {
+
+  // 1 - define the errors
+  NSError *noAccountsError = [NSError errorWithDomain:RWTwitterInstantDomain
+                                                 code:RWTwitterInstantErrorNoTwitterAccounts
+                                             userInfo:nil];
+  
+  NSError *invalidResponseError = [NSError errorWithDomain:RWTwitterInstantDomain
+                                                      code:RWTwitterInstantErrorInvalidResponse
+                                                  userInfo:nil];
+  
+  // 2 - create the signal block
+  @weakify(self)
+  void (^signalBlock)(RACSubject *subject) = ^(RACSubject *subject) {
+    @strongify(self);
+    
+    // 3 - create the request
+    SLRequest *request = [self requestforTwitterSearchWithText:text];
+    
+    // 4 - supply a twitter account
+    NSArray *twitterAccounts = [self.accountStore accountsWithAccountType:self.twitterAccountType];
+    if (twitterAccounts.count == 0) {
+      [subject sendError:noAccountsError];
+      return;
+    }
+    [request setAccount:[twitterAccounts lastObject]];
+    
+    // 5 - perform the request
+    [request performRequestWithHandler: ^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+      if (urlResponse.statusCode == 200) {
+        
+        // 6 - on success, parse the response
+        NSDictionary *timelineData = [NSJSONSerialization JSONObjectWithData:responseData
+                                                                     options:NSJSONReadingAllowFragments
+                                                                       error:nil];
+        [subject sendNext:timelineData];
+        [subject sendCompleted];
+      }
+      else {
+        // 7 - send an error on failure
+        [subject sendError:invalidResponseError];
+      }
+    }];
+  };
+  
+  RACSignal *signal = [RACSignal startLazilyWithScheduler:[RACScheduler scheduler]
+                                                    block:signalBlock];
+  
+  return signal;
+}
+
 
 
 - (BOOL)isValidSearchText:(NSString *)text {
